7f1b67ac1387c8c75a7473b215761db3f406f3c4
Sachin Limbu <sachinlimbu@outlook.com>
2019-05-08 15:54:19 +0100

    Add data access project to solution

---
+++ WireBrainCoffee.ShopInfoTool/WireBrainCoffee.DataAccess/CoffeeShopDataProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WireBrainCoffee.DataAccess.Model;
+
+namespace WireBrainCoffee.DataAccess
+{
+    public class CoffeeShopDataProvider
+    {
+        public IEnumerable<CoffeeShop> LoadCoffeeShops()
+        {
+            yield return new CoffeeShop { Location = "Frankfrut", BeansInStockInKg = 107 };
+            yield return new CoffeeShop { Location = "Freiburg", BeansInStockInKg = 23 };
+            yield return new CoffeeShop { Location = "Munich", BeansInStockInKg = 56 };
+        }
+    }
+}
---
+++ WireBrainCoffee.ShopInfoTool/WireBrainCoffee.DataAccess/Model/CoffeeShop.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WireBrainCoffee.DataAccess.Model
+{
+    public class CoffeeShop
+    {
+        public string Location { get; set; }
+        public int BeansInStockInKg { get; set; }
+    }
+}
