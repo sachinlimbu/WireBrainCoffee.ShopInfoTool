272f10b72ffcbd58f5d8aae65ad4d64b023746d0
Sachin Limbu <sachinlimbu@outlook.com>
2019-05-08 15:54:37 +0100

    Implement help command

--- WireBrainCoffee.ShopInfoTool/WireBrainCoffee.ShopInfoTool/Program.cs
+++ WireBrainCoffee.ShopInfoTool/WireBrainCoffee.ShopInfoTool/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using WireBrainCoffee.DataAccess;
 
 namespace WireBrainCoffee.ShopInfoTool
 {
@@ -7,6 +8,24 @@ namespace WireBrainCoffee.ShopInfoTool
         static void Main(string[] args)
         {
             Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
+            Console.WriteLine("Write 'help' to list available commands");
+
+            var coffeeShopDataProvider = new CoffeeShopDataProvider();
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+                var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
+
+                if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("> Available coffee shop commands:");
+                    foreach (var coffeeShop in coffeeShops)
+                    {
+                        Console.WriteLine($">" + coffeeShop.Location);
+                    }
+                }
+            }
         }
     }
 }
